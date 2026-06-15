a4b35993f123a201fd9a3b124523b9919ce0a285
Natan Ayalo <natanayalo@gmail.com>
2026-06-15 23:41:59 +0300

    fix(migrations): optimize batch_op column addition and constrain naming

--- db/migrations/versions/a44580010250_add_proposal_type_to_proposals.py
+++ db/migrations/versions/a44580010250_add_proposal_type_to_proposals.py
@@ -17,26 +17,22 @@ def _check_condition(column_name: str, values: Iterable[str]) -> str:
 
 
 def upgrade() -> None:
-    op.add_column(
-        "proposals",
-        sa.Column(
-            "proposal_type",
-            sa.Enum(
-                "scout",
-                "reflection",
-                name="proposal_type",
-                native_enum=False,
-                create_constraint=False,
-            ),
-            server_default="scout",
-            nullable=False,
-        ),
-    )
-
     with op.batch_alter_table("proposals") as batch_op:
-        batch_op.create_index(
-            batch_op.f("ix_proposals_proposal_type"), ["proposal_type"], unique=False
+        batch_op.add_column(
+            sa.Column(
+                "proposal_type",
+                sa.Enum(
+                    "scout",
+                    "reflection",
+                    name="proposal_type",
+                    native_enum=False,
+                    create_constraint=False,
+                ),
+                server_default="scout",
+                nullable=False,
+            )
         )
+        batch_op.create_index(op.f("ix_proposals_proposal_type"), ["proposal_type"], unique=False)
         batch_op.create_check_constraint(
             op.f("ck_proposals_proposal_type"),
             _check_condition("proposal_type", ["scout", "reflection"]),
@@ -46,5 +42,5 @@ def upgrade() -> None:
 def downgrade() -> None:
     with op.batch_alter_table("proposals") as batch_op:
         batch_op.drop_constraint(op.f("ck_proposals_proposal_type"), type_="check")
-        batch_op.drop_index(batch_op.f("ix_proposals_proposal_type"))
+        batch_op.drop_index(op.f("ix_proposals_proposal_type"))
         batch_op.drop_column("proposal_type")
