6a663ceb3bd7a2590dac44cceebcde6901380aa3
Natan Ayalo <natanayalo@gmail.com>
2026-06-18 08:27:20 +0300

    fix: include squash merges in changelog config

--- tests/unit/test_changelog_config.py
+++ tests/unit/test_changelog_config.py
@@ -16,7 +16,11 @@ def test_changelog_config_tracks_merged_pull_requests() -> None:
         {
             "pattern": "Merge pull request #([0-9]+) from [^\\n]+\\n\\n(.+)",
             "replace": "${2} ([#${1}](https://github.com/natanayalo/code-agent/pull/${1}))",
-        }
+        },
+        {
+            "pattern": "(.*) \\(#([0-9]+)\\)",
+            "replace": "${1} ([#${2}](https://github.com/natanayalo/code-agent/pull/${2}))",
+        },
     ]
 
     parser_messages = [parser.get("message") for parser in config["git"]["commit_parsers"]]
